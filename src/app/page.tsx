a4018766486944b52319429aee3cfe96fb4d2cb4
Natalie Hoang <nataliehoang@stupid-Macbook-Pro-M3-Max.local>
2024-10-10 13:57:38 +0700

    conditionally render UI based on isSignedIn check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { getServerCookie } from "@/utils/cookiesActions";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
-const Home = () => {
+const Home = async () => {
+	const userName = await getServerCookie("userName");
+	const jobTitle = await getServerCookie("jobTitle");
+
+	const isSignedIn = !!userName && !!jobTitle;
 	return (
 		<Box bg="bg-accent" minH="100vh">
 			<Container maxW="container.lg" py={10}>
 				<Heading as="h1" size="2xl" textAlign="center" mb={4}>
 					Welcome to Anime Explorer
 				</Heading>
+				{isSignedIn ? (
+					<Text>Welcome, {userName}!</Text>
+				) : (
+					<Text>Please sign in to continue.</Text>
+				)}
 			</Container>
 		</Box>
 	);
