23a37348f06ff38c712dc74a02dcd1a46a410404
Natalie Hoang <nataliehoang@stupid-Macbook-Pro-M3-Max.local>
2024-10-10 14:15:37 +0700

    modify Navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
 "use client";
 
 import { deleteServerCookie } from "@/utils/cookiesActions";
-import { Box, Button, Container, Flex, Text } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+	Box,
+	Button,
+	Container,
+	Divider,
+	Flex,
+	HStack,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Text,
+	useColorModeValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import ColorModeToggle from "./common/ColorModeToggle";
@@ -17,20 +31,50 @@ const Navbar = ({ isSignedIn, userName, jobTitle }: NavbarProps) => {
 		await deleteServerCookie("jobTitle");
 		redirect("/");
 	};
+
+	const bgColor = useColorModeValue(
+		"rgba(255, 255, 255, 0.8)",
+		"rgba(26, 32, 44, 0.8)",
+	);
 	return (
-		<Box as="nav" color="text" boxShadow="md">
+		<Box
+			as="nav"
+			bg={bgColor}
+			color="text"
+			boxShadow="sm"
+			position="fixed"
+			top={0}
+			left={0}
+			right={0}
+			zIndex={1000}
+		>
 			<Container maxW="container.xl" py={4}>
 				<Flex justify="space-between" align="center">
 					<Link href="/" passHref>
 						<Button variant="ghost">Home</Button>
 					</Link>
-					<ColorModeToggle />
-					<Text>{userName}</Text>
-					<Text>{jobTitle}</Text>
-					<Button onClick={handleSignOut}>Sign Out</Button>
-					<Link href="/profile">
-						<Button>Profile</Button>
-					</Link>
+					<HStack spacing={4}>
+						<ColorModeToggle />
+						{isSignedIn ? (
+							<Menu>
+								<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+									<Text as="span" fontWeight="medium" mr={2}>
+										{userName}
+									</Text>
+									<Text as="span" fontSize="sm" color="gray.500">
+										{jobTitle}
+									</Text>
+								</MenuButton>
+								<MenuList>
+									<Link href="/profile" passHref legacyBehavior>
+										<MenuItem>Profile</MenuItem>
+									</Link>
+									<Divider />
+									<MenuItem onClick={handleSignOut}>Log Out</MenuItem>
+								</MenuList>
+							</Menu>
+						) : null}
+					</HStack>
 				</Flex>
 			</Container>
 		</Box>
