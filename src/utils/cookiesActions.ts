486a5a37ee1726ffb0f3ab40cee71dd0198af0c7
Natalie Hoang <nataliehoang@stupid-Macbook-Pro-M3-Max.local>
2024-10-10 13:32:56 +0700

    create server cookies actions

---
+++ src/utils/cookiesActions.ts
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+
+export const getServerCookie = async (name: string) => {
+	const cookieStore = cookies();
+	return cookieStore.get(name)?.value || "";
+};
+
+export const setServerCookie = async (name: string, value: string) => {
+	const cookieStore = cookies();
+	cookieStore.set(name, value, {
+		path: "/",
+	});
+};
+
+export const deleteServerCookie = async (name: string) => {
+	const cookieStore = cookies();
+	cookieStore.set(name, "", {
+		path: "/",
+		maxAge: 0,
+	});
+};
